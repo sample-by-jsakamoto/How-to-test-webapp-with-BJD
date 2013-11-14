38fce29654893b59631df81d9ae9fdea8d338e43
jsakamoto <junichi.jp@gmail.com>
2013-11-14 16:47:42 +0900

    1st commit.

---
+++ SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RegistrationRepository Repository { get; set; }
+
+        public HomeController()
+        {
+            this.Repository = new RegistrationRepository();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AttendeeRegistration registration)
+        {
+            var id = this.Repository.Regist(registration);
+            return RedirectToAction("Complete", new { registration.Id });
+        }
+
+        public ActionResult Complete(Guid id)
+        {
+            var registration = this.Repository.Find(id);
+            return View(registration);
+        }
+    }
+}
---
+++ SampleWebApp/Models/RegistrationRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebApp.Models
+{
+    public class RegistrationRepository
+    {
+        private Dictionary<Guid, AttendeeRegistration> GetStrage()
+        {
+            const string storeKey = "a97c431e-295d-4055-844d-f24e1e5683a3";
+            var appStore = HttpContext.Current.Application;
+            lock (appStore)
+            {
+                if (appStore.AllKeys.Contains(storeKey) == false)
+                {
+                    appStore.Add(storeKey, new Dictionary<Guid, AttendeeRegistration>());
+                }
+                return appStore[storeKey] as Dictionary<Guid, AttendeeRegistration>;
+            }
+        }
+
+        public Guid Regist(AttendeeRegistration registration)
+        {
+            var store = GetStrage();
+            lock (store)
+            {
+                registration.Id = Guid.NewGuid();
+                store.Add(registration.Id, registration);
+                return registration.Id;
+            }
+        }
+
+        public AttendeeRegistration Find(Guid id)
+        {
+            var store = GetStrage();
+            lock (store)
+            {
+                return store[id];
+            }
+        }
+    }
+}
